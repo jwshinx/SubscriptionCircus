e1928a7e8a807874bdb6c8aae56790adf681bc23
Joel Shin <joelshin@pratik-doshis-iMac.local>
2012-11-16 16:25:40 -0800

    more initialization specs

--- spec/subscription_helper.rb
+++ spec/subscription_helper.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe "Subscription" do
+
  describe "creation" do
-  before { @sub = Subscription.new valid_subscription_attributes } 
-  #let(:sub) { Subscription.new 'Monthly', Date.today, Date.today, true, 'James Bond', '123 Main St', '101' }
-  #subject
+  let(:sub) { Subscription.new valid_subscription_attributes } 
   it "returns frequency" do
-   @sub.frequency.should == 'Monthly'
+   sub.frequency.should == 'Monthly' 
   end
-  it "returns xxx" do
-   @sub.to_s
+  it "returns name" do
+   sub.name.should == 'James Bond'
+  end
+  it "returns anniversary date" do
+   sub.anniversary_date.should == Date.today
+  end
+  it "returns start-auto-invoicing date" do
+   sub.start_auto_invoicing_date.should == Date.today
+  end
+  it "returns address" do
+   sub.address.should == '1 Main St'
+  end
+  it "returns apartment unit" do
+   sub.unit.should == '101'
   end
  end
+
 end
 
 def valid_subscription_attributes
