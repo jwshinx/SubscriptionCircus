1f7941d99a564d78a5314d0f2fbf1b43267bb109
Joel Shin <jwshin@gmail.com>
2012-11-24 11:10:21 -0800

    added more specs

--- spec/customer_rating_spec.rb
+++ spec/customer_rating_spec.rb
@@ -2,15 +2,18 @@ require 'spec_helper'
 require 'world'
 
 describe "CustomerRating" do
+ let(:elton_rating) { FactoryGirl.build( :customer_rating_for_elton ) }
+ let(:paul_rating) { FactoryGirl.build( :customer_rating_for_paul ) }
 
  describe "for elton who owes $50" do
-  let(:customer_rating_for_elton) { FactoryGirl.build( :customer_rating_for_elton ) }
-  it { customer_rating_for_elton.should == 'C' }
+  it { elton_rating.should == 'C' }
  end
  describe "for paul who owes $500" do
-  let(:customer_rating_for_paul) { FactoryGirl.build( :customer_rating_for_paul ) }
-  it { customer_rating_for_paul.should == 'D' }
+  it { paul_rating.should == 'D' }
  end
-
+ describe "comparing elton and paul" do
+  it { elton_rating.better_than?(paul_rating).should be_true }
+ end
 end
+
 
--- spec/payment_rating_spec.rb
+++ spec/payment_rating_spec.rb
@@ -4,12 +4,18 @@ describe "PaymentRating" do
  let(:mark) { FactoryGirl.build( :payment_20_of_50_rated ) }
  let(:sam) { FactoryGirl.build( :payment_45_of_50_rated ) }
  describe "for mark who has paid $25 of $50" do
-  it { mark.should == 'D' }
+  subject { mark }
+  it { should == 'D' }
  end
  describe "for sam who has paid $45 of $50" do
-  it { sam.should == 'C' }
+  subject { sam }
+  it { should == 'C' }
  end
  describe "comparing sam and mark" do
+  subject { sam }
   it { sam.better_than?( mark ).should be_true }
  end
+
+ #its(:name){ should be_present }
+
 end
